ecd55316eea1920c2b3b576b71e3f570e1d271f9
NBryant83 <nakisha.bryant@gmail.com>
2021-02-10 07:37:07 -0500

    added to js and html

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 This is a spin-off of the simple game Go Fish, except with emojis. I picked this because playing Go Fish with my mother and siblings is something I vividly remember and thought it would be nice to make something similar that other people can play.
 
 ## Game Rules
-User always plays first and clicks on the fishbowl to start the game
+User always plays first and clicks on start to start the game
 User clicks on an emoji they'd like to match to request it from the computer
 If computer has the particular emoji, they give it/them to user and user gets to request again
 If computer does not have it, user must 'go fish' and click on the fishbowl to be dealt an emoji
@@ -17,6 +17,7 @@ The game is won by whoever has matched and run out of emojis by the time the fis
 ## User Stories
 When the page loads, there is a game board with three sections. The left and the right are where the user and computers emojis will be, and the middle sections has a 'Reset' button, the fishpile, and a message area.
 When the page loads, the game starts and 5 emojis are dealt to me and the computer from the fishpile
+I can see the emojis I have on hand
 I can click on one of my emojis to request all of the computer's matching emojis
 If computer has it, I get it and am able to request another
 If computer doesn't have it, I have to click on the fishbowl to get another emoji
--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 /* Constants */
+// how many emojis are in the game in total
+const TOTAL_HAND = 40
 
 /* Game Logic Variables and State */
 let humanHand = [] // this represents the emojis in the human hand
@@ -6,16 +8,17 @@ let aiHand = [] //
 let fishbowl = [] // this represents how many emojis have not been played yet
 let humanMatchPile = [] // this represents how many matches the human has
 let aiMatchPile = [] // 
+let messages = ('')
 
 /* DOM References */
 // reset button 
 const resetButtonEl = document.querySelector('#reset')
 
 // left section
-const lSectionEl = document.querySelector('#left-section')
+const humanMatchPileEl = document.querySelector('#human-match-pile')
 
 // right section
-const rSectionEl = document.querySelector('#right-section')
+const aiMatchPileEl = document.querySelector('#ai-match-pile')
 
 // message area
 const messagesEl = document.querySelector('#messages')
@@ -23,31 +26,116 @@ const messagesEl = document.querySelector('#messages')
 // fishbowl
 const fishbowlEl = document.querySelector('#fishbowl')
 
-// player hand
-//const playerHandEl =
+// human hand
+const humanHandEl = document.querySelector('#human-hand')
+
+// ai hand
+const aiHandEl = document.querySelector('#ai-hand')
 
 /* Functions and Game Logic */
+// function to start game
+// fishbowl is full
+// humanHandEl and aiHandEl each have 5 emojis
+// humanMatchPileEl and aiMatchPileEl are empty
+// messagesEl is showing gameplay instructions
+// appendChild 
+// math.random, math.floor, for loop for emojis
+// show instructions in console div
+// fill player emoji sections
 const initialize = (event) => {
+    humanHand = []
+    aiHand = []
+    fishbowl = []
+    humanMatchPile = []
+    aiMatchPile = []
 
+    // filling the fishbowl with 40 emojis
+    for (let i = 0; i < 4; i++) {
+        fishbowl.push('😾', '🐶', '🐘', '🦄', '🐮', '🦁', '🦚', '🐬', '🐳', '🐁');
+    }
+    // this for loop is to draw human and ai 5 initial emojis
+    for (let i = 0; i < 5; i++) {
+        humanHand.push(drawEmoji())
+        aiHand.push(drawEmoji())
+    }
+    // appendChild to show instructions in messagesEl
+
+
+
+    // appendChild to fill in emoji sections
+
+
+    // function to clear messages in console
+    // clearMessages();
+
+    // console.log(fishbowl);
+    // console.log('initialize');
 }
 
+// function to clear the messages display
+const clearMessages = () => {
+    while (messagesEl.firstChild) {
+        messagesEl.removeChild(messagesEl.firstChild)
+    }
+}
+
+
+// function to draw random emoji
+// needs to run for ai and human
+// takes random emoji out of fishbowl and returns it
+const drawEmoji = () => {
+    let randomIndex = Math.floor(Math.random() * fishbowl.length)
+    let randomEmoji = fishbowl.splice(randomIndex, 1)[0]
+        // console.log(`This is the emoji we removed: ${randomEmoji}`)
+        // console.log(`${fishbowl}`)
+    return randomEmoji
+}
+
+// function for human to receive another emoji
+// player clicks on fishbowl, an emoji is depleted from fishbowl and added to player's emojis
+// if it is requested emoji, player gets it and goes again
+// else if it is not requested emoji, player gets it and ai requests emoji from player
+// if any player has nothing in hand, they must receive an emoji
+// math.random, math.floor, for loop for emojis
+// must append messages to console
 const goFish = (event) => {
 
 }
 
-const requestEmoji = (event) => {
+// function for what happens when it is ai's turn
+// ai requests emoji, humanHand gives it up/tells ai to goFish (automatic process)
+// game reverts to human's turn
+// must append messages to console
+const aiTurn = (event) => {
 
 }
+
+// function for what happens when human wins
+// gameplay is ceased
+// 'You win', 'click reset to start new game' is appended in messages div
+// stretch: emojis perform a parade in div as well
+const humanWins = (event) => {
+
+}
+
+// function for what happens when ai wins
+// gameplay is ceased
+// 'The computer wins', 'click reset to start new game' is appended in messages div
+// stretch: ai emojis parade in div as well
+const aiWins = (event) => {
+
+}
+
 
 /* Event Listeners */
 // DOMContentLoaded - this is going to run the initialize function
 document.addEventListener('DOMContentLoaded', initialize)
 
 // fishbowl click - this is going to start the game
-fishbowlEL.addEventListener('click', goFish)
+fishbowlEl.addEventListener('click', goFish)
 
 // reset click - this restarts the game
 resetButtonEl.addEventListener('click', initialize)
 
 // player hand click - this selects emoji user is requesting
-playerHandEl.addEventListener('click', requestEmoji)
+humanHandEl.addEventListener('click', goFish)
